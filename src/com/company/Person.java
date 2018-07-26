dce6bb4a8eff282e512f5c56a2303112ef39bc74
Denis <papa149@mail.ru>
2018-07-26 15:39:48 +0500

    Second commit for interactive rebase

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -46,5 +46,6 @@ public class Person {
 
     public void TryToFly(){
         System.out.println("My dream"); //first commit fot interactive rebase
+        System.out.println("is"); //second commit for interactive rebase
     }
 }
