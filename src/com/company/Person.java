8e80a6ce7e74bce7e4e9cf2f284d8c6a7d43b5f6
Denis <papa149@mail.ru>
2018-07-26 15:55:44 +0500

    First commit for interactive rebase (attempt 3)

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -45,8 +45,6 @@ public class Person {
     }
 
     public void TryToFly(){
-        System.out.println("My dream"); //first commit fot interactive rebase
-        System.out.println("is"); //second commit for interactive rebase
-        System.out.println("to fly"); // third commit for interactive rebase
+        System.out.println("My dream"); //first commit fot interactive rebase (attempt 3)
     }
 }
